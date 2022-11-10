be8fefa4fc597969a204c6188a68484d7ed74c5e
Charity K. Mutembei <100141458+Charity-Mutembei@users.noreply.github.com>
2022-11-10 17:38:26 +0300

    Feature (about page): about as page style and links install

--- joboapp/urls.py
+++ joboapp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path 
 from . import views
-urlpatterns={
-    path('', views.welcome)
-} 
+urlpatterns=[
+    path('', views.welcome, name='home'),
+    path('/about', views.about, name='about'),
+    path('/stories', views.stories, name='stories')
+]
--- joboapp/views.py
+++ joboapp/views.py
@@ -5,3 +5,9 @@ from django.http import HttpResponse
 def welcome(request):
     # return HttpResponse("You did it!")
     return render (request, 'index.html')
+
+def about(request):
+    return render (request, 'about.html')
+
+def stories(request):
+    return render (request, 'stories.html')
