6f5803a0a3711e5d3547ebc09d821d73d375edda
Charity K. Mutembei <100141458+Charity-Mutembei@users.noreply.github.com>
2022-11-10 23:57:06 +0300

    Feature (chat rooms): chat rooms install and connected to admin page

--- joboapp/models.py
+++ joboapp/models.py
@@ -1,3 +1,13 @@
+from email.policy import default
+from unittest.util import _MAX_LENGTH
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+class Room(models.Model):
+    name = models.CharField(max_length=255000)
+
+class Message(models.Model):
+    value = models.CharField(max_length=255000)
+    date = models.DateTimeField(default=datetime.now, blank=True)
+    user = models.CharField(max_length=2550000)
+    room = models.CharField(max_length=255000000)
--- joboapp/urls.py
+++ joboapp/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('', views.welcome, name='home'),
-    path('/about', views.about, name='about'),
-    path('/stories', views.stories, name='stories')
+    path('about', views.about, name='about'),
+    path('stories', views.stories, name='stories'),
+    path ('<str:room>/', views.room, name='room'),
+    path ('checkview', views.checkview, name='checkview'),
+    path ('send', views.send, name='send'),
+    path ('getMessages/<str:room>/', views.getMessages, name='getMessages'),
+
 ]
